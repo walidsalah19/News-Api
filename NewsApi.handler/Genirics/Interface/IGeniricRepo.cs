71104bd32c7ff771254be6fa7987b7bfa0faa593
walid salah <walidsalah9585@gmail.com>
2025-04-27 02:16:04 +0300

    add

---
+++ NewsApi.handler/Genirics/Interface/IGeniricRepo.cs
@@ -0,0 +1,22 @@
+﻿using NewsApi.Domain.AppEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApi.handler.Genirics.Interface
+{
+    public interface IGeniricRepo<T> where T:BaseEntity
+    {
+        Task<T> GetbyIdAsync(int id);
+
+        Task<IEnumerable<T>> GetAllWithAsync();
+
+        Task AddAsync(T item);
+
+        void DeleteAsync(T item);
+
+        void Update(T item);
+    }
+}
---
+++ NewsApi.handler/Genirics/Interface/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using NewsApi.Domain.AppEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApi.handler.Genirics.Interface
+{
+    public interface IUnitOfWork: IAsyncDisposable
+    {
+
+        Task<int> CompleteAsync();
+
+        IGeniricRepo<T> Repository<T>() where T : BaseEntity;
+
+        void Detach<T>(T entity) where T : BaseEntity;
+    }
+}
