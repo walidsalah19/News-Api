6ab000fd849f1280c5eb6c56637ecdd7eb4b7603
walid salah <walidsalah9585@gmail.com>
2025-04-28 03:12:26 +0300

    updates

---
+++ NewsApi.handler/Mapper/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using NewsApi.Domain.AppEntity;
+using NewsApi.Domain.Dtos.BannerDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApi.handler.Mapper
+{
+   public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Banners, ReadBanner>();
+            CreateMap<WriteBanner, Banners>();
+
+        }
+    }
+}
